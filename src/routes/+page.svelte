dd8e566a53a996dbd8a9453e7f38633025e55b24
gachagajnr <williampius17@gmail.com>
2024-03-20 02:59:51 +0300

    add hero content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,16 @@
 <div class="container mx-auto max-w-screen-xl min-h-screen text-center justify-center">
 	<div class="hero min-h-screen bg-base-200">
-  <div class="hero-content text-center">
-    <div class="max-w-5xl">
-      <h1 class="text-6xl font-bold">welcome to nerve web solutions</h1>
-      <p class="py-6 text-4xl font-light">
-        We are a software solutions agency based in Nairobi, Kenya who specialise in crafting bespoke,
-		digital products for our happy clients.</p>
-     </div>
-  </div>
-</div>
-	 
+		<div class="hero-content text-center">
+			<div class="max-w-5xl">
+ 				<h1 class="text-6xl font-bold">A Warm Welcome to the Nerve Web Solutions</h1>
+				<p class="py-6 text-4xl font-light">
+					We are a software solutions agency based in Nairobi, Kenya who specialise in crafting
+					bespoke, digital products for our happy clients.
+				</p>
+			</div>
+		</div>
+	</div>
+
 	<h1>Intro</h1>
 	A brief intro with a few key strong points
 	<h1>How We Work</h1>
