a0a49ea2045bc860e4953a461819413fb875127e
gachagajnr <williampius17@gmail.com>
2024-03-20 02:22:49 +0300

    styling home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<div class="min-h-screen">
-	<h1 class="text-3xl">Welcome to Sense activ web designers</h1>
+<div class="container mx-auto max-w-screen-xl min-h-screen text-center justify-center">
+	<h1 class="text-6xl py-6 font-bold">Welcome to Sense Web</h1>
 </div>
