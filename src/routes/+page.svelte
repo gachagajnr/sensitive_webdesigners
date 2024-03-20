2c09583214da6a1ea0fc738ed97b102040ea636a
gachagajnr <williampius17@gmail.com>
2024-03-20 22:52:12 +0300

    complete how we work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -67,83 +67,83 @@
 		</div>
 	</div>
 	<div class="py-12">
-		<h1 class="text-center text-4xl">How We Work</h1>
-		<div class="flex flex-row justify-center gap-6 py-6 mx-4">
-			<div class="container mx-auto w-3/4 p-2 md:flex hidden">
-				<img src={Design} class="object-cover rounded-lg" alt="how we work" />
-			</div>
-			<div class=" md:w-full">
-				<ul class="steps steps-vertical">
-					<li class="step step-primary py-2">
-						<div
-							class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
-						>
-							<h1 class="text-xl">Initial Consultation</h1>
-							<p class="p-1 text-pretty">
-								We kick off every project with a thorough initial consultation. This is where we sit
-								down with you, preferably over a cup of tea, to truly understand your business, your
-								goals, and your challenges. We believe that listening is the key to delivering
-								tailored solutions that address your unique needs.
-							</p>
-						</div>
-					</li>
-					<li class="step step-primary py-2">
-						<div
-							class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
-						>
-							<h1 class="text-xl">Needs Assessment</h1>
-							<p class="p-1 text-pretty">
-								Once we have a solid understanding of your business and objectives, we conduct a
-								comprehensive needs assessment. This involves analyzing your current digital
-								presence, identifying pain points, and uncovering opportunities for improvement. By
-								delving deep into your requirements, we ensure that our solutions are precisely
-								aligned with your goals.
-							</p>
-						</div>
-					</li>
-					<li class="step step-primary py-2">
-						<div
-							class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
-						>
-							<h1 class="text-xl">Planning and Preparation</h1>
-							<p class="p-1 text-pretty">
-								Before diving into development, we invest time in meticulous planning and
-								preparation. This involves outlining project specifications, defining milestones,
-								and assembling the right team for the job. By setting clear objectives and
-								expectations upfront, we ensure a smooth and efficient development process.
-							</p>
-						</div>
-					</li>
-					<li class="step step-primary py-2">
-						<div
-							class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
-						>
-							<h1 class="text-xl">Agile Development</h1>
-							<p class="p-1 text-pretty">
-								We embrace agile development methodologies to ensure flexibility and adaptability
-								throughout the project lifecycle. Our iterative approach allows us to respond
-								quickly to changes, incorporate feedback, and deliver high-quality results on time
-								and within budget. We keep you involved every step of the way, providing regular
-								updates and opportunities for collaboration
-							</p>
-						</div>
-					</li>
-					<li class="step step-primary py-2">
-						<div
-							class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
-						>
-							<h1 class="text-xl">Continuous Support</h1>
-							<p class="p-1 text-pretty">
-								Our commitment to your success doesn't end with project delivery. We provide
-								continuous support and maintenance to ensure that your solution remains up-to-date,
-								secure, and optimized for performance. Whether you need technical assistance,
-								updates, or enhancements, our team is always here to help.
-							</p>
-						</div>
-					</li>
-				</ul>
+		<h1 class="text-center text-4xl py-2">How We Work</h1>
+		<div class="hero min-h-screen bg-base-200">
+			<div class="hero-content text-center">
+				<div class="max-w-2xl">
+					<ul class="steps steps-vertical">
+						<li data-content="✓" class="step py-2">
+							<div
+								class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
+							>
+								<h1 class="text-xl">Initial Consultation</h1>
+								<p class="p-1 text-pretty">
+									We kick off every project with a thorough initial consultation. This is where we
+									sit down with you, preferably over a cup of tea, to truly understand your
+									business, your goals, and your challenges. We believe that listening is the key to
+									delivering tailored solutions that address your unique needs.
+								</p>
+							</div>
+						</li>
+						<li data-content="✓" class="step py-2">
+							<div
+								class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
+							>
+								<h1 class="text-xl">Needs Assessment</h1>
+								<p class="p-1 text-pretty">
+									Once we have a solid understanding of your business and objectives, we conduct a
+									comprehensive needs assessment. This involves analyzing your current digital
+									presence, identifying pain points, and uncovering opportunities for improvement.
+									By delving deep into your requirements, we ensure that our solutions are precisely
+									aligned with your goals.
+								</p>
+							</div>
+						</li>
+						<li data-content="✓" class="step py-2">
+							<div
+								class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
+							>
+								<h1 class="text-xl">Planning and Preparation</h1>
+								<p class="p-1 text-pretty">
+									Before diving into development, we invest time in meticulous planning and
+									preparation. This involves outlining project specifications, defining milestones,
+									and assembling the right team for the job. By setting clear objectives and
+									expectations upfront, we ensure a smooth and efficient development process.
+								</p>
+							</div>
+						</li>
+						<li data-content="✓" class="step py-2">
+							<div
+								class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
+							>
+								<h1 class="text-xl">Agile Development</h1>
+								<p class="p-1 text-pretty">
+									We embrace agile development methodologies to ensure flexibility and adaptability
+									throughout the project lifecycle. Our iterative approach allows us to respond
+									quickly to changes, incorporate feedback, and deliver high-quality results on time
+									and within budget. We keep you involved every step of the way, providing regular
+									updates and opportunities for collaboration
+								</p>
+							</div>
+						</li>
+						<li data-content="✓" class="step py-2">
+							<div
+								class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
+							>
+								<h1 class="text-xl">Continuous Support</h1>
+								<p class="p-1 text-pretty">
+									Our commitment to your success doesn't end with project delivery. We provide
+									continuous support and maintenance to ensure that your solution remains
+									up-to-date, secure, and optimized for performance. Whether you need technical
+									assistance, updates, or enhancements, our team is always here to help.
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
+
 		<p class="text-2xl">
 			By following this structured and collaborative approach, we ensure that every project we
 			undertake is a success, delivering tangible results and driving your business forward in the
