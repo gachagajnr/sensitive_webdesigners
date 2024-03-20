2832a0756881b18566a240e833c5301c542493dc
gachagajnr <williampius17@gmail.com>
2024-03-20 03:14:22 +0300

    tow inport logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	<div class="hero min-h-screen bg-base-200">
 		<div class="hero-content text-center">
 			<div class="max-w-5xl">
- 				<h1 class="text-6xl font-bold">Welcome to the Nerve Web Solutions</h1>
+				<h1 class="text-6xl font-bold">Welcome to Nerve Web Solutions</h1>
 				<p class="py-6 text-4xl font-light">
 					We are a software solutions agency based in Nairobi, Kenya who specialise in crafting
 					bespoke, digital products for our happy clients.
@@ -10,7 +10,17 @@
 			</div>
 		</div>
 	</div>
-
+	<div class="w-full grid justify-center">
+		<div class="flex max-w-5xl text-center w-full justify-center">
+			<div class="text-center py-12">
+				<img src="" alt="logo" />
+				<h1 class="text-4xl font-light">
+					A reknowned software company offering cloud based mobile and web solutions dedicated to
+					making your personal or business success and productivity
+				</h1>
+			</div>
+		</div>
+	</div>
 	<h1>Intro</h1>
 	A brief intro with a few key strong points
 	<h1>How We Work</h1>
