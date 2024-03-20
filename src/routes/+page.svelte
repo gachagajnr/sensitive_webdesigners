cc70db4fe66cd49a4f91cccc3be8d21f4c6a04b0
gachagajnr <williampius17@gmail.com>
2024-03-21 01:15:31 +0300

    finish contact section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -211,52 +211,72 @@
 			</div>
 		</div>
 	</div>
-	<div class="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-		<div class="flex flex-col justify-center py-3">
-			<h1 class="text-center text-4xl">Contact Us</h1>
-			<div>
-				<p class="text-2xl">Leave us a Message</p>
+	<h1 class="text-center text-4xl">Contact Us</h1>
+	<p class="text-2xl">Leave us a Message</p>
 
-				<form method="POST" class="card p-3 bg-secondary grid justify-start gap-1">
-					<label>
-						Full Name
-						<input
-							name="name"
-							type="name"
-							required
-							class="input input-sm"
-							placeholder="Full Name"
-						/>
-					</label>
-					<label>
-						Email
-						<input name="email" type="email" required class="input input-sm" placeholder="Email" />
-					</label>
-					<label>
-						Phone
-						<input name="phone" type="phone" class="input input-sm" placeholder="Phone(Optional)" />
-					</label>
-					<label>
-						Subject
-						<input
-							name="subject"
-							type="text"
-							required
-							class="input input-sm"
-							placeholder="Subject"
-						/>
-					</label>
-					<label>
-						Message
-						<textarea name="message" required class="textarea textarea-xs" placeholder="Message" />
-					</label>
-					<button>Send</button>
-				</form>
-			</div>
+	<div class="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+		<div class="  grid justify-end p-2">
+			<form
+				method="POST"
+				class="card shadow-lg rounded-lg bg-base-100 p-4 grid justify-start gap-1"
+			>
+				<label class="text-sm text-start">
+					<span class="font-semibold">Full Name </span>
+					<input
+						name="name"
+						type="name"
+						required
+						class="input input-primary input-sm w-full"
+						placeholder="Full Name"
+					/>
+				</label>
+				<label class="text-sm text-start">
+					<span class="font-semibold">Email address </span>
+
+					<input
+						name="email"
+						type="email"
+						required
+						class="input input-primary input-sm w-full"
+						placeholder="Email"
+					/>
+				</label>
+				<label class="text-sm text-start">
+					<span class="font-semibold">Phone </span>
+					<input
+						name="phone"
+						type="phone"
+						class="input input-primary input-sm w-full"
+						placeholder="Phone(Optional)"
+					/>
+				</label>
+				<label class="text-sm text-start">
+					<span class="font-semibold">Subject </span>
+
+					<input
+						name="subject"
+						type="text"
+						required
+						class="input input-primary input-sm w-full"
+						placeholder="Subject"
+					/>
+				</label>
+				<label class="text-sm text-start">
+					<span class="font-semibold">Message </span>
+
+					<textarea
+						name="message"
+						required
+						rows="4"
+						class="textarea textarea-primary w-full textarea-xs"
+						placeholder="Message"
+					/>
+				</label>
+				<button class="btn btn-primary w-full">Send</button>
+			</form>
 		</div>
 		<div class="justify-center items-center gap-2 p-2">
 			<div class="card p-3 grid gap-3 justify-start">
-				
 				<div class="join gap-3 cursor-pointer">
 					<svg
 						class="w-6 h-6 text-gray-800"
@@ -297,7 +317,7 @@
 
 					<p class="join-item font-semibold">Follow on Facebook</p>
 				</div>
-				
+
 				<div class="join gap-3 cursor-pointer">
 					<svg
 						class="w-6 h-6 text-gray-800"
@@ -311,7 +331,7 @@
 							d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
 						/>
 					</svg>
-					<p class="join-item font-semibold ">Follow on Instagram</p>
+					<p class="join-item font-semibold">Follow on Instagram</p>
 				</div>
 				<div class="join gap-3 cursor-pointer">
 					<svg
@@ -346,6 +366,14 @@
 					</svg>
 
 					<p class="join-item font-semibold">nervewebsolutions@gmail.com</p>
+				</div>
+				<div class="card grid gap-1 justify-center text-start">
+					<img src={Logo} width={60} height={60} alt="logo" class="join-item" />
+					<h2 class="text-xl font-semibold text-neutral">Nerve Web Solutions</h2>
+
+					<h2 class="font-bold text-sm">KIUU Containers</h2>
+					<h2 class="font-bold text-sm">Nairobi Kenya</h2>
+					<h2 class="font-bold text-sm">0706335880</h2>
 				</div>
 			</div>
 		</div>
