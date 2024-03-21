fa08dee43346eea076907a84ed2484019363cfa9
gachagajnr <williampius17@gmail.com>
2024-03-21 20:20:25 +0300

    modify projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,7 @@
 <div class="container mx-auto">
 	<div class="mt-16">
-		<h1 class="text-4xl font-bold">Projects We've worked on</h1>
-		<p class="p-4 max-w-4xl text-xl">
+		<h1 class="text-4xl text-secondary font-bold">Projects We've worked on</h1>
+		<p class="p-4 max-w-4xl  ">
 			At Nerve Web Solutions, we take pride in the diverse range of projects we've had the privilege
 			to work on. Here are a few examples of the impactful projects we've successfully delivered:
 		</p>
