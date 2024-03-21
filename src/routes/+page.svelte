58449ad76389e21731265c62d581daa60ab2da35
gachagajnr <williampius17@gmail.com>
2024-03-21 17:32:12 +0300

    finished how we work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,93 +65,151 @@
 			</div>
 		</div>
 	</div>
-	<div class="py-12">
-		<h1 class="text-center text-4xl py-2">How We Work</h1>
-		<div class="hero min-h-screen bg-base-200">
-			<div class="hero-content text-center">
-				<div class="max-w-2xl">
-					<ul class="steps steps-vertical">
-						<li data-content="✓" class="step py-2">
-							<div
-								class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
+
+	<div class="grid justify-center items-center mt-12">
+		<div class=" grid justify-center">
+			<h1 class="text-center text-4xl py-4 text-primary">How We Work</h1>
+			<div class="max-w-4xl">
+				<ul class="timeline timeline-vertical">
+					<li>
+						<div class="timeline-start border border-info timeline-box">
+							<h1 class="text-xl text-secondary">Initial Consultation</h1>
+
+							We kick off every project with a thorough initial consultation. This is where we sit
+							down with you, preferably over a cup of tea, to truly understand your business, your
+							goals, and your challenges. We believe that listening is the key to delivering
+							tailored solutions that address your unique needs.
+						</div>
+						<div class="timeline-middle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="w-5 h-5 text-primary"
+								><path
+									fill-rule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+									clip-rule="evenodd"
+								/></svg
 							>
-								<h1 class="text-xl">Initial Consultation</h1>
-								<p class="p-1 text-pretty">
-									We kick off every project with a thorough initial consultation. This is where we
-									sit down with you, preferably over a cup of tea, to truly understand your
-									business, your goals, and your challenges. We believe that listening is the key to
-									delivering tailored solutions that address your unique needs.
-								</p>
-							</div>
-						</li>
-						<li data-content="✓" class="step py-2">
-							<div
-								class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
+						</div>
+						<hr class="bg-accent" />
+					</li>
+					<li>
+						<hr class="bg-accent" />
+						<div class="timeline-middle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="w-5 h-5 text-primary"
+								><path
+									fill-rule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+									clip-rule="evenodd"
+								/></svg
 							>
-								<h1 class="text-xl">Needs Assessment</h1>
-								<p class="p-1 text-pretty">
-									Once we have a solid understanding of your business and objectives, we conduct a
-									comprehensive needs assessment. This involves analyzing your current digital
-									presence, identifying pain points, and uncovering opportunities for improvement.
-									By delving deep into your requirements, we ensure that our solutions are precisely
-									aligned with your goals.
-								</p>
-							</div>
-						</li>
-						<li data-content="✓" class="step py-2">
-							<div
-								class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
+						</div>
+						<div class="timeline-end timeline-box border border-info">
+							<h1 class="text-xl text-secondary">Needs Accessment</h1>
+
+							Once we have a solid understanding of your business and objectives, we conduct a
+							comprehensive needs assessment. This involves analyzing your current digital presence,
+							identifying pain points, and uncovering opportunities for improvement. By delving deep
+							into your requirements, we ensure that our solutions are precisely aligned with your
+							goals.
+						</div>
+						<hr class="bg-secondary" />
+					</li>
+					<li>
+						<hr class="bg-secondary" />
+						<div class="timeline-start border border-secondary timeline-box">
+							<h1 class="text-xl text-secondary">Planning & Preparation</h1>
+
+							Before diving into development, we invest time in meticulous planning and preparation.
+							This involves outlining project specifications, defining milestones, and assembling
+							the right team for the job. By setting clear objectives and expectations upfront, we
+							ensure a smooth and efficient development process.
+						</div>
+						<div class="timeline-middle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="w-5 h-5 text-primary"
+								><path
+									fill-rule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+									clip-rule="evenodd"
+								/></svg
 							>
-								<h1 class="text-xl">Planning and Preparation</h1>
-								<p class="p-1 text-pretty">
-									Before diving into development, we invest time in meticulous planning and
-									preparation. This involves outlining project specifications, defining milestones,
-									and assembling the right team for the job. By setting clear objectives and
-									expectations upfront, we ensure a smooth and efficient development process.
-								</p>
-							</div>
-						</li>
-						<li data-content="✓" class="step py-2">
-							<div
-								class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
+						</div>
+						<hr class="bg-info" />
+					</li>
+					<li>
+						<hr class="bg-info" />
+
+						<div class="timeline-middle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="w-5 h-5"
+								><path
+									fill-rule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+									clip-rule="evenodd"
+								/></svg
 							>
-								<h1 class="text-xl">Agile Development</h1>
-								<p class="p-1 text-pretty">
-									We embrace agile development methodologies to ensure flexibility and adaptability
-									throughout the project lifecycle. Our iterative approach allows us to respond
-									quickly to changes, incorporate feedback, and deliver high-quality results on time
-									and within budget. We keep you involved every step of the way, providing regular
-									updates and opportunities for collaboration
-								</p>
-							</div>
-						</li>
-						<li data-content="✓" class="step py-2">
-							<div
-								class="text-start p-4 shadow-md bg-base-200 rounded-lg hover:-translate-y-1 hover:cursor-pointer hover:bg-base-300 duration-150"
+						</div>
+						<div class="timeline-end  border border-accent timeline-box">
+							<h1 class="text-xl text-info">Agile Development</h1>
+
+							We embrace agile development methodologies to ensure flexibility and adaptability
+							throughout the project lifecycle. Our iterative approach allows us to respond quickly
+							to changes, incorporate feedback, and deliver high-quality results on time and within
+							budget. We keep you involved every step of the way, providing regular updates and
+							opportunities for collaboration
+						</div>
+						<hr class="bg-primary" />
+					</li>
+					<li>
+						<hr class="bg-primary" />
+						<div class="timeline-start border border-primary timeline-box">
+							<h1 class="text-xl text-primary">Continuous Support</h1>
+
+							Our commitment to your success doesn't end with project delivery. We provide
+							continuous support and maintenance to ensure that your solution remains up-to-date,
+							secure, and optimized for performance. Whether you need technical assistance, updates,
+							or enhancements, our team is always here to help.
+						</div>
+						<div class="timeline-middle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="w-5 h-5"
+								><path
+									fill-rule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+									clip-rule="evenodd"
+								/></svg
 							>
-								<h1 class="text-xl">Continuous Support</h1>
-								<p class="p-1 text-pretty">
-									Our commitment to your success doesn't end with project delivery. We provide
-									continuous support and maintenance to ensure that your solution remains
-									up-to-date, secure, and optimized for performance. Whether you need technical
-									assistance, updates, or enhancements, our team is always here to help.
-								</p>
-							</div>
-						</li>
-					</ul>
-				</div>
+						</div>
+					</li>
+				</ul>
 			</div>
 		</div>
-		<div class="grid justify-center py-3">
-			<p class="text-2xl">
+		<div class=" mt-6 p-6">
+			<p class="text-2xl font-light">
 				By following this structured and collaborative approach, we ensure that every project we
 				undertake is a success, delivering tangible results and driving your business forward in the
 				digital landscape
 			</p>
 		</div>
 	</div>
-	<div class="py-12">
-		<h1 class="text-center text-4xl">Our Focus</h1>
+	<div class="mt-6 p-6">
+		<h1 class="text-center text-secondary text-4xl">Our Focus</h1>
 		<div class="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
 			<div
 				class="card bg-base-300 shadow-md rounded-lg hover:-translate-y-2 duration-150 hover:bg-base-100"
