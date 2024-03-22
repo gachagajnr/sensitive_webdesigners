122e950813fcf795756ca3ed62f10bbd3ec392a4
gachagajnr <williampius17@gmail.com>
2024-03-22 22:56:30 +0300

    edit what we focus on

--- src/routes/focus/+page.svelte
+++ src/routes/focus/+page.svelte
@@ -1,4 +1,15 @@
 <div class="container mx-auto">
-    <h1>Focus</h1>
-
+	<div class="grid gap-4 mt-16">
+		<div>
+			<h1 class="text-4xl font-bold text info">What we Focus On</h1>
+			<h1 class="text-xl p-4">
+				We understand that every business is unique, with its own set of challenges, goals, and
+				aspirations. That's why our approach is centered around creating tailored digital solutions
+				that fit the specific needs of each client we work with. Our commitment to excellence and
+				innovation drives us to go above and beyond to deliver solutions that not only meet but
+				exceed our clients' expectations, paving the way for their success in the digital landscape.
+			</h1>
+		</div>
+		<div></div>
+	</div>
 </div>
