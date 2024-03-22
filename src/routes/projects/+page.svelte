ea7674a1a6d314e9d881a1e976a7845c8841dbdb
gachagajnr <williampius17@gmail.com>
2024-03-22 16:10:11 +0300

    modify projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,9 +1,105 @@
 <div class="container mx-auto">
-	<div class="mt-16">
-		<h1 class="text-4xl text-secondary font-bold">Projects We've worked on</h1>
-		<p class="p-4 max-w-4xl  ">
-			At Nerve Solutions, we take pride in the diverse range of projects we've had the privilege
-			to work on. Here are a few examples of the impactful projects we've successfully delivered:
-		</p>
+	<div class="grid mt-16 justify-center items-center gap-4">
+		<div class="grid justify-center max-w-5xl">
+			<h1 class="text-4xl text-primary font-bold">Projects We've worked on</h1>
+			<p class="p-4  text-xl">
+				At Nerve Solutions, we take pride in the diverse range of projects we've had the privilege
+				to work on. Here are a few examples of the impactful projects we've successfully delivered:
+			</p>
+		</div>
+		<div class="flex flex-row flex-wrap justify-center p-6 gap-4 items-center">
+			<div
+				class="card w-96 border border-secondary hover:bg-secondary-content hover:bg-opacity-30 cursor-pointer"
+			>
+				<div class="card-body items-start">
+					<h2 class="card-title">MahaliKenya Mobile App</h2>
+					<p>Ios/Android Mobile App made with Flutter.</p>
+					<div class="card-actions">
+						<a
+							href="https://play.google.com/store/apps/details?id=com.mahalikenya.app.mahali&hl=en&gl=US"
+							class="font-semibold text-info"
+						>
+							MahaliKenya
+						</a>
+					</div>
+				</div>
+			</div>
+			<div
+				class="card w-96 border border-secondary hover:bg-secondary-content hover:bg-opacity-40 cursor-pointer"
+			>
+				<div class="card-body items-start">
+					<h2 class="card-title">ookulima</h2>
+					<p>ookulima Blogs Ecommerce and Blog Web Application</p>
+					<div class="card-actions">
+						<a href="https://ookulima.vercel.app/" class="font-semibold text-info">
+							ookulima Blogs
+						</a>
+					</div>
+				</div>
+			</div>
+			<div
+				class="card w-96 border border-secondary hover:bg-secondary-content hover:bg-opacity-30 cursor-pointer"
+			>
+				<div class="card-body items-start">
+					<h2 class="card-title">Mahali Kenya Landing Page</h2>
+					<p>Website and Landing Page for MahaliKenya mobile application.</p>
+					<div class="card-actions">
+						<a href="https://mahalikenya.vercel.app" class="text-info font-semibold">
+							Mahalikenya
+						</a>
+					</div>
+				</div>
+			</div>
+			<div
+				class="card w-96 border border-secondary hover:bg-secondary-content hover:bg-opacity-30 cursor-pointer"
+			>
+				<div class="card-body items-start">
+					<h2 class="card-title">Michoro Art</h2>
+					<p>Interior Decor Art Marketplace platform for artists and Ecommerce Website</p>
+					<div class="card-actions">
+						<a href="https://michoro-postgres.vercel.app/" class="font-semibold text-info">
+							Michoro Art
+						</a>
+					</div>
+				</div>
+			</div>
+			<div
+				class="card w-96 border border-secondary hover:bg-secondary-content hover:bg-opacity-30 cursor-pointer"
+			>
+				<div class="card-body items-start">
+					<h2 class="card-title">Wirerent</h2>
+					<p>Rental Management System</p>
+					<div class="card-actions">
+						<a href="https://wirerent.vercel.app" class="text-info text-lg"> Wirerent </a>
+					</div>
+				</div>
+			</div>
+			<div
+				class="card w-96 border border-secondary hover:bg-secondary-content hover:bg-opacity-30 cursor-pointer"
+			>
+				<div class="card-body items-start">
+					<h2 class="card-title">Rentaspace</h2>
+					<p>Properties Listing Website and Application</p>
+					<div class="card-actions">
+						<a href="https://luxury-rent.vercel.app/" class="font-semibold text-info">
+							Luxury Rent
+						</a>
+					</div>
+				</div>
+			</div>
+			<div
+				class="card w-96 border border-secondary hover:bg-secondary-content hover:bg-opacity-30 cursor-pointer"
+			>
+				<div class="card-body items-start">
+					<h2 class="card-title">Nerve Solutions</h2>
+					<p>Business Website for Nerve Solutions</p>
+					<div class="card-actions">
+						<a href="https://nerve-webs.vercel.app/" class="font-semibold text-info">
+							Nerves Solutions
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
