794766cfbf3b562dd7a9a0ae3f753e17b337c7f4
gachagajnr <williampius17@gmail.com>
2024-03-23 01:11:23 +0300

    done with contact us styling

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,72 +1,147 @@
 <div class="container mx-auto">
-	<div class="mt-16">
+	<div class="flex flex-col mt-16 justify-center gap-6">
 		<div class="">
-			<h1 class="text-center text-4xl">Work With Us</h1>
-			<p class="text-3xl">Leave us a Message</p>
-			<div class="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-				<div class="  grid justify-end p-4">
-					<form
-						method="POST"
-						class="card shadow-lg rounded-lg bg-base-300 p-4 grid justify-start gap-y-1.5"
-					>
-						<label class="text-sm text-start">
-							<span class="font-semibold text-sm">Full Name </span>
-							<input
-								name="name"
-								type="name"
-								required
-								class="input input-sm w-full"
-								placeholder="Full Name"
-							/>
-						</label>
-						<label class="text-sm text-start">
-							<span class="font-semibold text-sm">Email address </span>
-
-							<input
-								name="email"
-								type="email"
-								required
-								class="input input-sm w-full"
-								placeholder="Email"
-							/>
-						</label>
-						<label class="text-sm text-start">
-							<span class="font-semibold text-sm">Phone </span>
-							<input
-								name="phone"
-								type="phone"
-								class="input input-sm w-full"
-								placeholder="Phone(Optional)"
-							/>
-						</label>
-						<label class="text-sm text-start">
-							<span class="font-semibold text-sm">Subject </span>
-
-							<input
-								name="subject"
-								type="text"
-								required
-								class="input input-sm w-full"
-								placeholder="Subject"
-							/>
-						</label>
-						<label class="text-sm text-start">
-							<span class="font-semibold text-sm">Message </span>
-
-							<textarea
-								name="message"
-								required
-								rows="4"
-								class="textarea w-full textarea-xs"
-								placeholder="Message"
-							/>
-						</label>
-						<button class="btn btn-secondary w-full">Send</button>
-					</form>
+			<h1 class="font-bold text-4xl ">
+				Let's Connect and Turn Your  Vision into Reality.
+			</h1>
+		</div>
+		<div class="  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 justify-center p-4">
+			<div class="p-6">
+				<div class="ml-2 py-2 text-xl">
+					We're passionate about helping businesses thrive in the digital landscape. Whether you
+					have a question about our services, want to discuss a potential project, or simply want to
+					say hello, we'd love to hear from you!
 				</div>
-				<div class="justify-center items-center gap-2 p-2">Info</div>
+				<div class="ml-2 grid">
+					<p class="text-secondary text-xl font-bold p-1">Why Contact Us?</p>
+					<p class="ml-2 font-thin join gap-2">
+						<svg
+							class="w-6 h-6 text-gray-800 dark:text-white join-item"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							fill="currentColor"
+							viewBox="0 0 24 24"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<span class="join-item"> Personalized Attention </span>
+					</p>
+					<p class="ml-2 font-thin join gap-2">
+						<svg
+							class="w-6 h-6 text-gray-800 dark:text-white join-item"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							fill="currentColor"
+							viewBox="0 0 24 24"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<span class="join-item"> Expert Advice </span>
+					</p>
+					<p class="ml-2 font-thin join gap-2">
+						<svg
+							class="w-6 h-6 text-gray-800 dark:text-white join-item"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							fill="currentColor"
+							viewBox="0 0 24 24"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<span class="join-item">Transparent Communication </span>
+					</p>
+					<p class="text-secondary text-xl font-bold p-1">How to Reach Us</p>
+					<p class="ml-2 font-thin">
+						<span class="text-info">Phone:</span> Give us a call at
+						<span class="text-info">0706335880</span> to speak directly with a member of our team.
+					</p>
+					<p class="ml-2 font-thin">
+						<span class="text-info">Email:</span> Shoot us an email at
+						<span class="text-info">nervesolutions@gmail.com</span>, and we'll get back to you as
+						soon as possible.
+					</p>
+					<p class="ml-2 font-thin">
+						<span class="text-info"> Contact Form </span> Fill out the contact form with your details
+						and inquiry, and we'll reach out to you promptly.
+					</p>
+				</div>
+			</div>
+			<div class="flex justify-center">
+				<form method="POST" class="card w-3/4 bg-base-300 p-6  justify-center gap-2">
+					<h1 class="font-bold py-1 text-2xl text-secondary">Leave a Message</h1>
+					<label class="text-sm text-start ">
+						<span class="font-semibold text-sm">Full Name </span>
+						<input
+							name="name"
+							type="name"
+							required
+							class="input input-sm w-full"
+							placeholder="Full Name"
+						/>
+					</label>
+					<label class="text-sm text-start">
+						<span class="font-semibold text-sm">Email address </span>
+
+						<input
+							name="email"
+							type="email"
+							required
+							class="input input-sm w-full"
+							placeholder="Email"
+						/>
+					</label>
+					<label class="text-sm text-start">
+						<span class="font-semibold text-sm">Phone </span>
+						<input
+							name="phone"
+							type="number"
+							class="input input-sm w-full"
+							placeholder="Phone(Optional)"
+						/>
+					</label>
+					<label class="text-sm text-start">
+						<span class="font-semibold text-sm">Subject </span>
+
+						<input
+							name="subject"
+							type="text"
+							required
+							class="input input-sm w-full"
+							placeholder="Subject"
+						/>
+					</label>
+					<label class="text-sm text-start">
+						<span class="font-semibold text-sm">Message </span>
+
+						<textarea
+							name="message"
+							required
+							rows="4"
+							class="textarea w-full textarea-xs"
+							placeholder="Message"
+						/>
+					</label>
+					<button class="btn btn-neutral w-full">Submit</button>
+				</form>
 			</div>
 		</div>
 	</div>
-	<div></div>
 </div>
