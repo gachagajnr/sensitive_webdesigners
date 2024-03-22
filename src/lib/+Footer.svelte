27252423503d33554546b547603fc5018d565533
gachagajnr <williampius17@gmail.com>
2024-03-22 23:54:40 +0300

    mod footer

--- src/lib/+Footer.svelte
+++ src/lib/+Footer.svelte
@@ -4,7 +4,7 @@
 <footer class="footer items-center p-4 bg-neutral text-neutral-content">
   <aside class="items-center grid-flow-col">
     <img src={Logo} alt="logo" class="w-10 h-10"/>
-     <p>Copyright © Nerve web solutions 2024 - All right reserved</p>
+     <p>Copyright © Nerve Solutions 2024 - All right reserved</p>
   </aside> 
  
 </footer>
